d2abc05561fe7f2b2f9c8d1cb30b6288892241cb
chungfstem <Nguyenthimeochung@gmail.com>
2025-06-25 12:49:59 +0700

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -101,7 +101,7 @@ const Header = () => {
                 <>
                   <Button 
                      variant="outline"
-                    className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-semibold"
+                    className="bg-green-400 hover:bg-blue-500 text-gray-900 font-semibold"
                     onClick={() => handleAuthClick('register')}
                   >
                     Đăng nhập
